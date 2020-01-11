b192d326297d180cadd26b51a5fb142ee56d4c90
afroz282004 <afroz282004@gmail.com>
2020-01-11 15:32:49 +0530

    Add new page with new feature

---
+++ WebApp1/WebApp1/RepeterPagination.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp1
+{
+    public partial class RepeterPagination : System.Web.UI.Page
+    {
+        private int PageSize = 10;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                this.GetCustomersPageWise(1);
+            }
+
+
+        }
+        private void GetCustomersPageWise(int pageIndex)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetCustomersPageWise", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PageIndex", pageIndex);
+                    cmd.Parameters.AddWithValue("@PageSize", PageSize);
+                    cmd.Parameters.Add("@RecordCount", SqlDbType.Int, 4);
+                    cmd.Parameters["@RecordCount"].Direction = ParameterDirection.Output;
+                    con.Open();
+                    IDataReader idr = cmd.ExecuteReader();
+                    rptCustomers.DataSource = idr;
+                    rptCustomers.DataBind();
+                    idr.Close();
+                    con.Close();
+                    int recordCount = Convert.ToInt32(cmd.Parameters["@RecordCount"].Value);
+                    this.PopulatePager(recordCount, pageIndex);
+                }
+            }
+        }
+
+        private void PopulatePager(int recordCount, int currentPage)
+        {
+            double dblPageCount = (double)((decimal)recordCount / (decimal)PageSize);
+            int pageCount = (int)Math.Ceiling(dblPageCount);
+            List<ListItem> pages = new List<ListItem>();
+            if (pageCount > 0)
+            {
+                pages.Add(new ListItem("<<", "1", currentPage > 1));
+                if (currentPage != 1)
+                {
+                    pages.Add(new ListItem("Previous", (currentPage - 1).ToString()));
+                }
+                if (pageCount < 4)
+                {
+                    for (int i = 1; i <= pageCount; i++)
+                    {
+                        pages.Add(new ListItem(i.ToString(), i.ToString(), i != currentPage));
+                    }
+                }
+                else if (currentPage < 4)
+                {
+                    for (int i = 1; i <= 4; i++)
+                    {
+                        pages.Add(new ListItem(i.ToString(), i.ToString(), i != currentPage));
+                    }
+                    pages.Add(new ListItem("...", (currentPage).ToString(), false));
+                }
+                else if (currentPage > pageCount - 4)
+                {
+                    pages.Add(new ListItem("...", (currentPage).ToString(), false));
+                    for (int i = currentPage - 1; i <= pageCount; i++)
+                    {
+                        pages.Add(new ListItem(i.ToString(), i.ToString(), i != currentPage));
+                    }
+                }
+                else
+                {
+                    pages.Add(new ListItem("...", (currentPage).ToString(), false));
+                    for (int i = currentPage - 2; i <= currentPage + 2; i++)
+                    {
+                        pages.Add(new ListItem(i.ToString(), i.ToString(), i != currentPage));
+                    }
+                    pages.Add(new ListItem("...", (currentPage).ToString(), false));
+                }
+                if (currentPage != pageCount)
+                {
+                    pages.Add(new ListItem("next", (currentPage + 1).ToString()));
+                }
+                pages.Add(new ListItem(">>", pageCount.ToString(), currentPage < pageCount));
+            }
+            rptPager.DataSource = pages;
+            rptPager.DataBind();
+        }
+        protected void Page_Changed(object sender, EventArgs e)
+        {
+            int pageIndex = int.Parse((sender as LinkButton).CommandArgument);
+            this.GetCustomersPageWise(pageIndex);
+        }
+    }
+}
